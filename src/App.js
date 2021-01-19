11b9b637c55bdacb51fb9e00c0faa545da3edb1a
tstev <tstevens5@schools.nyc.gov>
2021-01-19 17:50:11 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+import image from './Components/Images/Bitmoji.png'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <h1 className="Title">Ms. Stevens</h1>
+      <h2 className= "Subtitle">Artist and Art Teacher</h2>
+      <img src = {image} alt= "Selfie" ></img>
+      <h1 className="School">FDNY High School</h1>
+      <p className="Content">About Me</p>
+      <p>Stuff that I've done or will do</p>
     </div>
+     
   );
 }
 
